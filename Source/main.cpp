ebeb39f092d0482b52b67d8416dade82c507f1d9
Crant <chrippe10@hotmail.com>
2012-11-19 16:54:07 +0100

    Added Mesh
    Added scale Human mesh

--- Source/main.cpp
+++ Source/main.cpp
@@ -46,8 +46,9 @@ void test()
 	eng->GetCamera()->LookAt(D3DXVECTOR3(30, 10, 10));
 
 	eng->CreateTerrain(D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(100, 1, 100), "Media/TerrainTexture.png", "Media/TerrainHeightmap.raw");
+	StaticMesh* scaleHuman = eng->CreateStaticMesh("Media/scale.obj", D3DXVECTOR3(5, -6, 15));
 	StaticMesh* bth = eng->CreateStaticMesh("Media/bth.obj", D3DXVECTOR3(5, 20, 15));
-	bth->Scale(0.1f);
+	scaleHuman->Scale(0.025f);
 	Light* testLight = eng->CreateLight(D3DXVECTOR3(15, 30, 15));
 
 	SoundEngine* seng = eng->GetSoundEngine();
