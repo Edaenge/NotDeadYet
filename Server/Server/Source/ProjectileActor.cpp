f099905ea6b4c545051836895d36e3894d59fdcf
azelio <ediz_cracked@hotmail.com>
2013-02-07 15:10:30 +0100

    * Changed some stuff with projectiles

--- Server/Server/Source/ProjectileActor.cpp
+++ Server/Server/Source/ProjectileActor.cpp
@@ -1,9 +1,11 @@
 #include "ProjectileActor.h"
 
-ProjectileActor::ProjectileActor(Actor* shooter, PhysicsObject* pObj) :
-	zShooter(shooter)
+ProjectileActor::ProjectileActor(Actor* owner, PhysicsObject* pObj) :
+	zOwner(owner), Actor()
 {
-	this->zPhysicsObject = pObj;
+	this->zPhysicsObject = pObj;	
+	this->zDir = owner->GetDir();
+	this->zPos = owner->GetPosition();
 }
 
 ProjectileActor::~ProjectileActor()
