63782d1e74810b35f42c974ff8c3376b7bb8fd2e
azelio <ediz_cracked@hotmail.com>
2013-02-15 16:45:31 +0100

    Fixed an error

--- Game/Source/NetworkMessageConverter.cpp
+++ Game/Source/NetworkMessageConverter.cpp
@@ -308,7 +308,7 @@ std::vector<std::string> NetworkMessageConverter::SplitMessage(const std::string
 	for(unsigned int i = 0; i < msg.length(); i++)
 	{
 		subMsg = "";
-		while (msg.at(i) != '*' || i >= msg.length())
+		while (msg.at(i) != '*' && i < msg.length())
 		{
 			subMsg += msg[i++];
 		}
