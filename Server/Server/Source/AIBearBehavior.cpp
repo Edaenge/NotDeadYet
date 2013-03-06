fa855a53d54dbab6f0767077959ee4e064d79a3d
Crant <chrippe10@hotmail.com>
2013-03-06 13:57:57 +0100

    Small change.

--- Server/Server/Source/AIBearBehavior.cpp
+++ Server/Server/Source/AIBearBehavior.cpp
@@ -304,9 +304,7 @@ Vector3 AIBearBehavior::ExaminePathfindingArea()
 			dest.x = cos(angle) * oldX - sin(angle) * oldY;
 			dest.z = cos(angle) * oldY + sin(angle) * oldX;
 			dest.Normalize();
-			dest = dest * (float)zFleeDistance;
-
-					
+			dest = dest * (float)zFleeDistance;			
 		}
 		else
 		{
