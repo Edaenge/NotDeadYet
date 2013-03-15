31ee57910f4bf80a69faa24a66f65b7d97e014f0
Rikard Johansson <rillee.johansson@gmail.com>
2013-03-15 15:34:38 +0100

    Push

--- Server/Server/Source/AIDeerBehavior.cpp
+++ Server/Server/Source/AIDeerBehavior.cpp
@@ -259,8 +259,8 @@ void AIDeerBehavior::SetFearLevel(float fear)
 
 bool AIDeerBehavior::InitPathfinder()
 {
-	//this->zPathfinder.InitAI(0.5,3840); //For big map.
-	this->zPathfinder.InitAI(0.5,90); //For small testing map.
+	this->zPathfinder.InitAI(0.5,3840); //For big map.
+	//this->zPathfinder.InitAI(0.5,90); //For small testing map.
 	this->zPathfinder.SetWorldPointer(this->zWorld);
 	return true;
 }
